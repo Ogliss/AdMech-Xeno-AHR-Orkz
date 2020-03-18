d08570a0bbb58fb767ce444b3e08868e998c536b
Ogliss <randomdragon2@hotmail.com>
2020-03-18 20:29:27 +0000

    Texture Fixes

--- 1.1/Source/AdeptusMechanicusXenoOrkz/Harmony/HarmonyInstance.cs
+++ 1.1/Source/AdeptusMechanicusXenoOrkz/Harmony/HarmonyInstance.cs
@@ -18,6 +18,7 @@ namespace AdeptusMechanicus.Orkz
         {
             Harmony harmony = new Harmony("rimworld.ogliss.adeptusmechanicus.orkz");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+            if (Prefs.DevMode) Log.Message(string.Format("Adeptus Mecanicus: Orkz: successfully completed {0} harmony patches.", harmony.GetPatchedMethods().Select(new Func<MethodBase, Patches>(Harmony.GetPatchInfo)).SelectMany((Patches p) => p.Prefixes.Concat(p.Postfixes).Concat(p.Transpilers)).Count((Patch p) => p.owner.Contains(harmony.Id))), false);
         }
     }
 }
